b5608a2adcb4fc1eea933411dbf914a6a8d78913
lovely <procodss@gmail.com>
2023-09-22 16:07:47 +0000

    ,

--- java/lovely.java
+++ java/lovely.java
@@ -1,150 +1,156 @@
 public class lovely {
-    public static void l(){
-    for(int i=1;i<=8;i++){
-        for(int j=1;j<=3;j++){
-            System.out.print("*");
-        }
-       
-        System.out.println(" ");
-    }
-    for(int i=1;i<=2;i++){
-         for(int k=1;k<=22;k++){
-             System.out.print("*");
-         }
-         System.out.println(" ");
-    }
-    return;
-}
-public static void o(){
-    for(int i=1;i<=2;i++){
-        for(int k=1;k<=22;k++){
-            System.out.print("*");
-        }
-        System.out.println(" ");
-   }
-   for(int i=1;i<=8;i++){
-    for(int j=1;j<=3;j++){
-        System.out.print("*");
-    }
-    for(int k=1;k<=16;k++){
-        System.out.print(" ");
-    }
-    for(int l=1;l<=2;l++){
-        System.out.print("*");
-    }
-    System.out.println("*");
-   }
-   for(int i=1;i<=2;i++){
-        for(int k=1;k<=22;k++){
-            System.out.print("*");
-        }
-        System.out.println(" ");
-   }
-   return;
-}
-public static void v(){
-    for(int i=1;i<=10;i++){
-        for(int j=1;j<=i-1;j++){
-            System.out.print(" ");
-        }
-        for(int k=1;k<=3;k++){
-            System.out.print("*");
-        }
-        for(int l=1;l<=10-i;l++){
-            System.out.print(" ");
-        }
-            for(int l=1;l<=10-i;l++){
-            System.out.print(" ");
-        }
-             for(int k=1;k<=3;k++){
-            System.out.print("*");
-        }
+    public static void l() {
+        for (int i = 1; i <= 8; i++) {
+            for (int j = 1; j <= 3; j++) {
+                System.out.print("*");
+            }
 
-        System.out.println(" ");
+            System.out.println(" ");
+        }
+        for (int i = 1; i <= 2; i++) {
+            for (int k = 1; k <= 22; k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+        return;
     }
 
-    return;
-}
-public static void e(){
-    for(int i=1;i<=2;i++){
-        for(int k=1;k<=22;k++){
-            System.out.print("*");
+    public static void o() {
+        for (int i = 1; i <= 2; i++) {
+            for (int k = 1; k <= 22; k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
         }
-        System.out.println(" ");
-   }
-   for(int i=1;i<=2;i++){
-    for(int j=1;j<=3;j++){
-        System.out.print("*");
+        for (int i = 1; i <= 8; i++) {
+            for (int j = 1; j <= 3; j++) {
+                System.out.print("*");
+            }
+            for (int k = 1; k <= 16; k++) {
+                System.out.print(" ");
+            }
+            for (int l = 1; l <= 2; l++) {
+                System.out.print("*");
+            }
+            System.out.println("*");
+        }
+        for (int i = 1; i <= 2; i++) {
+            for (int k = 1; k <= 22; k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+        return;
     }
-   
-    System.out.println(" ");
-}
-for(int i=1;i<=2;i++){
-        for(int k=1;k<=22;k++){
-            System.out.print("*");
+
+    public static void v() {
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 3; k++) {
+                System.out.print("*");
+            }
+            for (int l = 1; l <= 10 - i; l++) {
+                System.out.print(" ");
+            }
+            for (int l = 1; l <= 10 - i; l++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 3; k++) {
+                System.out.print("*");
+            }
+
+            System.out.println(" ");
         }
-        System.out.println(" ");
-   }
-   for(int i=1;i<=2;i++){
-    for(int j=1;j<=3;j++){
-        System.out.print("*");
+
+        return;
     }
-   
-    System.out.println(" ");
-}
-for(int i=1;i<=2;i++){
-        for(int k=1;k<=22;k++){
-            System.out.print("*");
+
+    public static void e() {
+        for (int i = 1; i <= 2; i++) {
+            for (int k = 1; k <= 22; k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
         }
-        System.out.println(" ");
-   }
-   return;
-}
-public static void y(){
-for(int i=1;i<=5;i++){
-        for(int j=1;j<=2*i-2;j++){
-            System.out.print(" ");
+        for (int i = 1; i <= 2; i++) {
+            for (int j = 1; j <= 3; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println(" ");
         }
-        for(int k=1;k<=3;k++){
-            System.out.print("*");
+        for (int i = 1; i <= 2; i++) {
+            for (int k = 1; k <= 22; k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
         }
-        for(int l=1;l<=11-2*i;l++){
-            System.out.print(" ");
+        for (int i = 1; i <= 2; i++) {
+            for (int j = 1; j <= 3; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println(" ");
         }
-            for(int l=1;l<=10-2*i;l++){
-            System.out.print(" ");
+        for (int i = 1; i <= 2; i++) {
+            for (int k = 1; k <= 22; k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
         }
-             for(int k=1;k<=3;k++){
-            System.out.print("*");
-        }
-       System.out.println(" ");
+        return;
     }
-    for(int i=1;i<=5;i++){
-        for(int j=1;j<=9;j++){
-            System.out.print(" ");
+
+    public static void y() {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 2 * i - 2; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 3; k++) {
+                System.out.print("*");
+            }
+            for (int l = 1; l <= 11 - 2 * i; l++) {
+                System.out.print(" ");
+            }
+            for (int l = 1; l <= 10 - 2 * i; l++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 3; k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
         }
-        for(int k=1;k<=4;k++){
-            System.out.print("*");
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 9; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 4; k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
         }
-        System.out.println(" ");
+        return;
     }
-    return;
-}
-public static void lovely(){
-    l();
-    System.out.println(" ");
-    o();
-    System.out.println(" ");
-    v();
-    System.out.println(" ");
-    e();
-    System.out.println(" ");
-    l();
-    System.out.println(" ");
-    y();
-return;
-}
-public static void main (String args[]){
-   lovely();
+
+    public static void lovely() {
+        l();
+        System.out.println(" ");
+        o();
+        System.out.println(" ");
+        v();
+        System.out.println(" ");
+        e();
+        System.out.println(" ");
+        l();
+        System.out.println(" ");
+        y();
+        return;
+    }
+
+    public static void main(String args[]) {
+        lovely();
     }
 }
